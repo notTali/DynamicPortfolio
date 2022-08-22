dea2202b29d96d4323ee40f9337df5df51a01740
Rilinde Ntsieni <NTSRIL002@myuct.ac.za>
2022-08-23 01:23:26 +0200

    Models

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,22 @@
+from pyexpat import model
+from turtle import mode, title
 from django.db import models
+import uuid
 
 # Create your models here.
+class Project(models.Model):
+    id = models.UUIDField(editable=False, default=uuid.uuid4, primary_key=True, null=False, blank=False)
+    title = models.CharField(max_length=200, null=False, blank=False)
+    description = models.CharField(max_length=200, null=False, blank=False)
+    tags = models.ManyToManyField('Tag')
+    image = models.ImageField(null=False, blank=False)
+
+    def __str__(self):
+        return self.title
+
+class Tag(models.Model):
+    id = models.UUIDField(editable=False, default=uuid.uuid4, primary_key=True, null=False, blank=False)
+    title = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title
